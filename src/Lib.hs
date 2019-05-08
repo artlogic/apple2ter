e800a5a94746b096bc05b54ae1f325c6b3aff921
James Kruth <james@kruth.org>
2019-05-08 11:36:25 -0400

    Turn the stub programs into something that identifies disks

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,23 @@
 module Lib
-    ( someFunc
+    ( readDisk
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Binary.Get
+import qualified Data.ByteString.Lazy as BL
+import Data.Word
+
+-- magic numbers for identifying disk types
+proDosDisk = 0x0138b0034c32a186
+dos33Disk = 0x01a527c909d018a5
+
+diskType :: Word64 -> String
+diskType magicNumber
+  | magicNumber == proDosDisk = "ProDOS"
+  | magicNumber == dos33Disk = "DOS 3.3"
+  | otherwise = "Unknown"
+
+readDisk :: FilePath -> IO ()
+readDisk path = do
+  rawBytes <- BL.readFile path
+  let magicNumber = runGet getWord64be rawBytes
+  putStrLn $ diskType magicNumber
